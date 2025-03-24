dd38a4b2bf52786dcbfd31bed666c8937a7af3bc
tugayjt <tugayjabbarov@gmail.com>
2025-03-24 21:36:49 +0400

    create configuration

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-a = {b}
+
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//       const enteredValue = inputEl.value;
+//       const listItemEl = document.createElement('li');
+//       listItemEl.textContent = enteredValue;
+//       listEl.appendChild(listItemEl);
+//       inputEl.value = '';
+
+// }
+
+// buttonEl.addEventListener('click',addGoal)
