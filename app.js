ed283389ad47aee3a45e0381ca8b4e5384ba6484
tugayjt <tugayjabbarov@gmail.com>
2025-03-26 01:09:00 +0400

    app.js file added

--- app.js
+++ app.js
@@ -12,3 +12,17 @@
 // }
 
 // buttonEl.addEventListener('click',addGoal)
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal(){
+//       const enteredValue = inputEl.value;
+//       const listItemEl = document.createElement('li');
+//       listItemEl.textContent = enteredValue;
+//       listEl.appendChild(listItemEl);
+//       inputEl.value = '';
+
+// }
+
+// buttonEl.addEventListener('click',addGoal)
