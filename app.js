fee8743cab42415dbbabff54dcaa0fa11fa85975
tugayjt <tugayjabbarov@gmail.com>
2025-04-01 00:38:36 +0400

    re-bulding

--- app.js
+++ app.js
@@ -14,14 +14,3 @@ Vue.createApp({
 
 
 
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// function addGoal(){
-//       const enteredValue = inputEl.value;
-//       const listItemEl = document.createElement('li');
-//       listItemEl.textContent = enteredValue;
-//       listEl.appendChild(listItemEl);
-//       inputEl.value = '';
-
