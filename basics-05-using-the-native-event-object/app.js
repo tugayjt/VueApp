dfe961d80e0560bd1c457e83d42a2ebab2884be0
tugayjt <tugayjabbarov@gmail.com>
2025-04-17 23:24:51 +0400

    Data Binding + Event Binding = Two-Way Binding

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -1,25 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
+      counter: 0,
       name: ''
     };
   },
   methods: {
-    confirmInput(){
-      this.confirmInput = this.name;
-    },
-    submitForm(){
-      alert("Submitted!");
-    },
-    setName(event,lastname) {
-      this.name = event.target.value + ' ' + lastname;
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+      // this.counter--;
     }
   }
 });
