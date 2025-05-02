a96c72b29da4e32115e1c68a7665a53a470532b6
tugayjt <tugayjabbarov@gmail.com>
2025-05-02 10:52:09 +0400

    Rendering Content Conditionally

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return{ 
+    enteredGoalValue: '',
+     goals: [] 
+    };
   },
+  methods: {
+    addGoal(){
+      this.goal.push(this.enteredGoalValue);
+    }
+  }
 });
 
 app.mount('#user-goals');
+
+
+if (true) {
+  // ...
+}
